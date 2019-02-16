d8501c21b5d566b5319e23c7f2143cebc392b5cb
FreeestyleR <pcholapasha@gmail.com>
2019-02-16 22:06:14 +0200

    начало функции для добавление простых чисел в массив

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+func natural(_ array: [Int], _ count: Int) -> [Int] {
+    let count1 = count
+    var natArray = array
+    if count1 != 1 {
+        var n = 2
+        while n <= count1 + 1 {
+            natArray.append(n)
+            n+=1
+        }
+    }
+    var c = 2
+    while c < count1 {
+        natArray.remove(at: c)
+        c+=2
+    }
+    return natArray
+}
+
+var naturalArray: [Int] = []
+print(natural(naturalArray, 1))
