7e26aeba0323a11961beab2fdc695c2f97fe8266
FreeestyleR <pcholapasha@gmail.com>
2019-02-17 22:13:58 +0200

    мсправления

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-func natural(_ array: [Int], _ count: Int) -> [Int] {
+//Задача №6
+//Звписать 100 простых чисел в массив
+
+func natural(_ count: Int) -> [Int] {
     let count1 = count
-    var natArray = array
+    var natArray: [Int] = []
     if count1 != 1 {
         var n = 2
         while n <= count1 + 1 {
@@ -10,13 +13,21 @@ func natural(_ array: [Int], _ count: Int) -> [Int] {
             n+=1
         }
     }
+    var p = natArray[0]
     var c = 2
+    
     while c < count1 {
-        natArray.remove(at: c)
-        c+=2
+        let d = c * p
+        natArray.remove(at: d)
+        c+=p
+    }
+    for i in natArray {
+        if natArray[i] > p {
+            p = natArray[i]
+        }
     }
     return natArray
 }
 
-var naturalArray: [Int] = []
-print(natural(naturalArray, 10))
+let arr: [Int] = natural(1)
+print(arr)
