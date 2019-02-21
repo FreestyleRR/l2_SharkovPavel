c2422e76e4f95be3deb7eaa7e308fae98975681a
FreeestyleR <pcholapasha@gmail.com>
2019-02-21 22:47:41 +0200

    qq

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -23,7 +23,7 @@ enum Kuzov {
     case chastKuz(kg: Double)
 }
 
-struct Hunday {
+struct lightCar {
     var brand: String
     var yearOfRelease: Int
     var obemKuz: Kuzov
@@ -35,14 +35,34 @@ struct Hunday {
         switch action {
         case .start:
             self.engine = .on
-        default:
+        case .turnOff:
             self.engine = .off
         }
-        
-        /* if action == .start {
-            self.engine = .on
-        } else if action == .turnOff {
-            self.engine = .off
-        }*/
-   }
+    }
 }
+
+struct heavyCar {
+    var brand: String
+    var yearOfRelease: Int
+    var obemKuz: Kuzov
+    var engine: Engine
+    var windows: Windows
+    var obemFullKuz: Double
+    
+    mutating func actionEng(action: actionEngine){
+        switch action {
+        case .start:
+            self.engine = .on
+        case .turnOff:
+            self.engine = .off
+        }
+    }
+}
+
+var carLight1 = lightCar(brand: "Honda", yearOfRelease: 2005, obemKuz: .pustKuz(empty: "Кузов пустой"), engine: .on, windows: .open, obemFullKuz: 36.5)
+var carLight2 = lightCar(brand: "Shkoda", yearOfRelease: 2012, obemKuz: .chastKuz(kg: 150.0), engine: .off, windows: .open, obemFullKuz: 45.2)
+
+var carHeavy1 = heavyCar(brand: "Kamaz", yearOfRelease: 1992, obemKuz: .fullKuz(full: "Кузов полный"), engine: .on, windows: .close, obemFullKuz: 700.0)
+var carHeavy2 = heavyCar(brand: "Laz", yearOfRelease: 1999, obemKuz: .chastKuz(kg: 434.2), engine: .on, windows: .close, obemFullKuz: 550.0)
+
+print("Легкоывя машина " + carLight1.brand + ", " + carLight1.yearOfRelease + " года выпуска. С объёмом кузова " + carLight1.obemFullKuz)
